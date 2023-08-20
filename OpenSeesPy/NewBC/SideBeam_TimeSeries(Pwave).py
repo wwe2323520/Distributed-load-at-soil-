121f76a1dcfdf166537aa7e5a7f62bb95fbb8945
wwe2323520 <106285345+wwe2323520@users.noreply.github.com>
2023-08-20 20:30:46 +0800

    Update SideBeam_TimeSeries(Pwave).py

--- OpenSeesPy/NewBC/SideBeam_TimeSeries(Pwave).py
+++ OpenSeesPy/NewBC/SideBeam_TimeSeries(Pwave).py
@@ -35,29 +35,31 @@ ws = 2*pi/ Ts
 
 
 time = np.arange(0,0.8001,1e-4)
-Vel_FF = np.zeros(len(time))
+PVel_FF = np.zeros(len(time))
+
+# -------- P WAVE -----------------
 Sideforce_y = np.zeros((len(time),100))
 Sideforce_x = np.zeros((len(time),100))
 
+    
 for k in range(len(time)):
     if k < 1001:
 # ------- For P wave ------------------------------------
-        Vel_FF[k] = np.sin(wp*time[k])/(-rho*cp*A)
-    # if k <= 1870:
-# # ------- For P wave ------------------------------------
-#         Vel_FF[k] = np.sin(ws*time[k])/(-rho*cs*A)
+        PVel_FF[k] = np.sin(wp*time[k])/(-rho*cp*A)
+step = 1000#1000
+Tstep = 2*step
         
 eta_s = rho*cs
-for i in range(100):
+eta_p = rho*cp
+for i in range(100):#100
     for j in range(len(time)): #len(time)
+# ------- For P wave ------------------------------------
         if j < 1001:
-            Sideforce_y[10*i+j,i] = -eta_s*Vel_FF[j]*A
+            Sideforce_y[10*i+j,i] = -eta_s*PVel_FF[j]*A
             Sideforce_x[10*i+j,i] = (nu/(1-nu))*np.sin(wp*time[j])
-
-        # if j <= 1870:
-        #     Sideforce[5555*i+j,i] = -2*rho*cp*Vel_FF[j]*A
-
-
+        if j >= 1000 and j < 2000:
+            Sideforce_y[10*i+j,i] = eta_s*PVel_FF[j-step]*A
+            Sideforce_x[10*i+j,i] = -(nu/(1-nu))*np.sin(wp*time[j-step])
 num_f = 100
 force_id = np.empty(num_f, dtype=object)
 for b in range(num_f):
@@ -65,7 +67,7 @@ for b in range(num_f):
             
 # ---- Velocity Freefield Plot ----------------
 # plt.figure()
-# plt.plot(time,Vel_FF)
+# plt.plot(time,SVel_FF)
 # plt.grid(True)
 
 
@@ -74,9 +76,11 @@ plt.rcParams["figure.figsize"] = (12, 8)
 plt.title("eleForce", fontsize = 18)
 plt.xlabel("tns(s)",fontsize=18)
 for h in range(100):    #101
-    # plt.plot(time,Sideforce_y[:,h],label = force_id[h])#force[4*i,:]
+# -------- P wave -----------------
+    plt.plot(time,Sideforce_y[:,h],label = force_id[h])#force[4*i,:]
     plt.plot(time,Sideforce_x[:,h],label = force_id[h])#force[4*i,:]
-    
+
+
 plt.grid(True)   
 plt.legend(loc='upper right',fontsize=10, ncol=5)
 plt.xlim(0,0.3)
@@ -86,24 +90,23 @@ plt.yticks(fontsize = 15)
 # ---- Output matrix eace column to txt file --------------
 num_rows, num_cols = Sideforce_y.shape# 8001,100
 # 建立資料夾
-folder_name_y = "Sideforce_y"
-folder_name_x = "Sideforce_x"
+# ---------- Pwave ---------------
+P_folder_name_y = "P_Sideforce_y"
+P_folder_name_x = "P_Sideforce_x"
 
-os.makedirs(folder_name_y, exist_ok=True)
+os.makedirs(P_folder_name_y, exist_ok=True)
 # 逐一建立txt檔案並放入資料夾
 for col in range(num_cols):
     column_values = Sideforce_y[:, col]
     output_file = f"ele{col + 1}.txt"
-    with open(os.path.join(folder_name_y, output_file), 'w') as f:
+    with open(os.path.join(P_folder_name_y, output_file), 'w') as f:
         for value in column_values:
             f.write(f"{value}\n")
 
-os.makedirs(folder_name_x, exist_ok=True)
+os.makedirs(P_folder_name_x, exist_ok=True)
 for col in range(num_cols):
     column_values = Sideforce_x[:, col]
     output_file = f"ele{col + 1}.txt"
-    with open(os.path.join(folder_name_x, output_file), 'w') as f:
+    with open(os.path.join(P_folder_name_x, output_file), 'w') as f:
         for value in column_values:
             f.write(f"{value}\n")
-            
-print(f"Txt files have been saved in the folder '{folder_name_y,folder_name_x}'.")
