ffb31365a78eb02561942bf7c31859daf6046c4f
wwe2323520 <106285345+wwe2323520@users.noreply.github.com>
2023-11-30 16:45:53 +0800

    Create OutPut_Dynamic.py

---
+++ OpenSeesPy/NewRefinement/Dynamic_File/OutPut_Dynamic.py
@@ -0,0 +1,5 @@
+# # ================= Create an output directory ===================
+# filename = 'soil10m_SideMidDash_Pwave'
+# if not os.path.exists(filename):
+#     os.makedirs(filename)
+# recorder('PVD', filename, 'vel','eleResponse','stresses') #'eleResponse','stresses'
