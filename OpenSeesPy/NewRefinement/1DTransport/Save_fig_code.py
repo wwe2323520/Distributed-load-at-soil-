2e8828cd356ca7103f5659ca9b1635f611dc7ec6
wwe2323520 <106285345+wwe2323520@users.noreply.github.com>
2025-07-10 15:26:21 +0800

    Create Save_fig_code.py

---
+++ OpenSeesPy/NewRefinement/1DTransport/Save_fig_code.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from matplotlib.ticker import LogLocator, NullFormatter, LogFormatter
+from matplotlib.ticker import MultipleLocator
+plt.rcParams['savefig.dpi'] = 300 # change dpi for all
+
+
+
+# fig1.savefig("D:/shiang/論文格式/期刊/國內結構期刊投稿/期刊圖片/Python plot/Swave_Different_BC_Compare.png", dpi=600, transparent=True) # transparent =True => make baceground color to 0
+fig1.savefig("D:/shiang/論文格式/Test_fig/Swave_Different_BC_Compare_300.png")
